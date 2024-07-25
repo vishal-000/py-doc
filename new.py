7049a369b489bb1d4a72c94d5f5b6fd89539c72e
vishal <vishal7284s@gmail.com>
2024-07-25 12:05:34 +0530

    added new

---
+++ new.py
@@ -0,0 +1,5 @@
+gender = input("what is your gender : ")
+if (gender == "male"):
+    print("Hello sir")
+else:
+    print("Hello madam")
---
+++ sample.py
@@ -0,0 +1,5 @@
+input("what is your gender : ")
+while (input == "male"):
+    print("Hello sir")
+else:
+    print("Hello madam")    
