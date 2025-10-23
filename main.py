92fea2cc6602806fb57f41d4bd5c54dfd4164358
vishal <vishal7284s@gmail.com>
2025-10-23 17:38:26 +0530

    m

--- main.py
+++ main.py
@@ -1,3 +1,26 @@
-tuple_list = {(2,5), (6,89)}
-max_list = max(item for tup in tuple_list for item in tup)
-print(max_list)
+def create_student_marks_dictionary():
+    student_data = {}
+    num_students = int(input("Enter the number of students :"))
+    for i in range(num_students):
+        name = input(f"Enter name for studen{i+1}:")
+        try:
+            marks = float(input(f"Enter marks for {name}:"))
+            student_data[name] = marks
+        except ValueError:
+            print("Invalid marks entered.Please enter a numeric value.")
+            continue
+    return student_data
+def display_student_marks(student_data):
+    while True:
+         search_name = input("\nEnter the name of the student to search for")
+         if search_name.lower() == 'quit':
+             break
+         if search_name in student_data:
+             print(f"Marks for {search_name}:{student_data[search_name]}")
+        else:
+            print(f"Student'{student_name}")
+
+if __name__ == "__main__":
+     print("\nComplete student marks dictionary :")
+     print (student_dict)
+        
